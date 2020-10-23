6c449f6a3b182a527ea77434e3340744e3c69950
ccarlosf <2238513100@qq.com>
2020-10-23 16:39:06 +0800

    SpringBoot 3. SpringBoot数据访问 3.3 SpringBoot整合Redis

--- springboot03_data/src/test/java/com/ccarlos/springboot/Springboot03DataApplicationTests.java
+++ springboot03_data/src/test/java/com/ccarlos/springboot/Springboot03DataApplicationTests.java
@@ -2,13 +2,17 @@ package com.ccarlos.springboot;
 
 import com.ccarlos.mapper.ArticleMapper;
 import com.ccarlos.mapper.CommentMapper;
+import com.ccarlos.pojo.Address;
 import com.ccarlos.pojo.Article;
 import com.ccarlos.pojo.Comment;
+import com.ccarlos.pojo.Person;
 import com.ccarlos.repository.CommentRepository;
+import com.ccarlos.repository.PersonRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -43,5 +47,35 @@ class Springboot03DataApplicationTests {
         if (byId.isPresent()) {
             System.out.println(byId.get());
         }
+    }
+
+    //测试整合redis
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    public void savePerson() {
+        Person person = new Person();
+        person.setFirstname("张");
+        person.setLastname("三");
+
+        Address address = new Address();
+        address.setCity("北京");
+        address.setCountry("中国");
+        person.setAddress(address);
+
+        // 向redis数据库中添加了数据
+        personRepository.save(person);
+
+    }
+
+    @Test
+    public void selectPerson() {
+        List<Person> list = personRepository.findByAddress_City("北京");
+        for (Person person : list) {
+            System.out.println(person);
+        }
+
+
     }
 }
