009f2651679bcfdeb3dc76fe3b31d4f6c1ed6fe4
ccarlosf <2238513100@qq.com>
2020-10-27 10:56:43 +0800

    SpringBoot 5. SpringBoot缓存管理 5.3 自定义Redis缓存序列化机制 5.3.2 自定义RedisCacheManager

--- springboot05_cache/src/main/java/com/ccarlos/config/RedisConfig.java
+++ springboot05_cache/src/main/java/com/ccarlos/config/RedisConfig.java
@@ -44,4 +44,32 @@ public class RedisConfig {
         return template;
     }
 
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // 分别创建String和JSON格式序列化对象，对缓存数据key和value进行转换
+        RedisSerializer<String> strSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jacksonSeial =
+                new Jackson2JsonRedisSerializer(Object.class);
+
+        // 解决查询缓存转换异常的问题
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jacksonSeial.setObjectMapper(om);
+
+        // 定制缓存数据序列化方式及时效
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofDays(1))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(strSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(jacksonSeial))
+                .disableCachingNullValues();
+        RedisCacheManager cacheManager = RedisCacheManager
+                .builder(redisConnectionFactory).cacheDefaults(config).build();
+        return cacheManager;
+    }
+
+
 }
